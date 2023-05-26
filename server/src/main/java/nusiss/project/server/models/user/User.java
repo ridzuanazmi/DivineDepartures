06e4d8a101801fe388f97c93353b50698f0a884a
ridzy <ridzuanazmi.11@gmail.com>
2023-05-26 10:27:48 +0800

    Small comment

--- server/src/main/java/nusiss/project/server/models/user/User.java
+++ server/src/main/java/nusiss/project/server/models/user/User.java
@@ -101,6 +101,7 @@ public class User implements UserDetails {
     }
 
     // UserDetails methods
+    // Returns a list of roles 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
